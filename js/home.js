a8e81289d3f41c1301a2e9ee217ae904b0b06c1d
kelvi5 <42703248+kelvi5@users.noreply.github.com>
2019-10-23 19:20:12 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -3,8 +3,8 @@ var trocar = 1;
 var i = 0;
 var fundo = document.getElementById('corpo');
 
-img[0] = "C:/Users/Gabs/Desktop/VendaValida/estagio_VendaValida/img/fundo1.jpg";
-img[1] = "C:/Users/Gabs/Desktop/VendaValida/estagio_VendaValida/img/fundo2.jpg";
+img[0] = "../img/fundo1.jpg";
+img[1] = "../img/fundo2.jpg";
 
 setInterval( function() {
     fundo.style.backgroundImage = 'url(' + img[i] + ')';
